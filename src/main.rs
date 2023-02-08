af80aa82c56a5d20fd731f725425fc13111c23e2
Knarkzel <knarkzel@gmail.com>
2023-02-08 19:10:26 +0100

    Better style

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,10 @@ fn app(cx: Scope) -> Element {
     // Get current day in year and entry
     let now = js_sys::Date::new_0();
     let start = js_sys::Date::new_with_year_month_day(now.get_full_year(), 0, 0);
-    let day = ((now.value_of() - start.value_of()) / 1000.0 / 60.0 / 60.0 / 24.0).floor() as usize - 1;
-    
+    let day =
+        ((now.value_of() - start.value_of()) / 1000.0 / 60.0 / 60.0 / 24.0).floor() as usize - 1;
+    let current_time = now.to_locale_time_string("no");
+
     cx.render(match spreadsheet {
         Ok(entries) => {
             // Get prayer times
@@ -37,17 +39,84 @@ fn app(cx: Scope) -> Element {
             let asr_shafi = float_to_time(entry.asr_shafi);
             let asr_hanafi = float_to_time(entry.asr_hanafi);
             let maghrib = float_to_time(entry.maghrib);
-            let isha = entry.isha.map(|it| float_to_time(it)).unwrap_or(String::from("--::--"));
-            
+            let isha = entry
+                .isha
+                .map(|it| float_to_time(it))
+                .unwrap_or(String::from("--::--"));
+
             rsx! {
                 h1 { "Mawaqeet" }
-                p { format!("Fajr: {fajr}") }
-                p { format!("Soloppgang: {shuroq}") }
-                p { format!("Dhuhr: {dhuhr}") }
-                p { format!("Asr Shafi: {asr_shafi}") }
-                p { format!("Asr Hanafi: {asr_hanafi}") }
-                p { format!("Maghrib: {maghrib}") }
-                p { format!("Isha: {isha}") }
+                p { format!("Klokka er {current_time}") }
+                table {
+                    thead {
+                        tr {
+                            th {
+                                "Bønn"
+                            }
+                            th {
+                                "Klokkeslett"
+                            }
+                        }                        
+                    }
+                    tbody {
+                        tr {
+                            td {
+                                "Fajr"
+                            }
+                            td {
+                                fajr
+                            }
+                        }
+                        tr {
+                            td {
+                                "Soloppgang"
+                            }
+                            td {
+                                shuroq
+                            }
+                        }
+                        tr {
+                            td {
+                                "Dhuhr"
+                            }
+                            td {
+                                dhuhr
+                            }
+                        }
+                        tr {
+                            td {
+                                "Asr Shafi"
+                            }
+                            td {
+                                asr_shafi
+                            }
+                        }
+                        tr {
+                            td {
+                                "Asr Hanafi"
+                            }
+                            td {
+                                asr_hanafi
+                            }
+                        }
+                        tr {
+                            td {
+                                "Maghrib"
+                            }
+                            td {
+                                maghrib
+                            }
+                        }
+                        tr {
+                            td {
+                                "Isha"
+                            }
+                            td {
+                                isha
+                            }
+                        }
+                    }
+                }
             }
         }
         Err(e) => rsx! { div { "Error while parsing spreadsheet: {e:?}" } },
