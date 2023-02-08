92e64da096c30625f06a2d9485ae1cc1c8542710
Knarkzel <knarkzel@gmail.com>
2023-02-08 18:28:23 +0100

    Parser working

--- src/spreadsheet.rs
+++ src/spreadsheet.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use std::io::Cursor;
-use calamine::{open_workbook, Error, RangeDeserializerBuilder, Reader, Xlsx};
+use calamine::{Error, RangeDeserializerBuilder, Reader, Xlsx};
 
 type Row = (
     String,
@@ -16,13 +16,14 @@ type Row = (
 
 #[derive(Debug)]
 pub struct Entry {
+    day: usize,
     fajr: f64,
     shurooq: f64,
     dhuhr: f64,
     asr_shafi: f64,
     asr_hanafi: f64,
     maghrib: f64,
-    isha: f64,
+    isha: Option<f64>,
 }
 
 pub fn parse(spreadsheet: &[u8]) -> Result<Vec<Entry>> {
@@ -42,28 +43,36 @@ pub fn parse(spreadsheet: &[u8]) -> Result<Vec<Entry>> {
         })
         .collect::<Vec<_>>();
 
-    // Group each row delimited by empty row, then map them into Month with name and entries
-    let months = rows
+    // Group each row delimited by empty row
+    let groups = rows
         .group_by(|first, second| first.0.len() > 0 && second.0.len() > 0)
         .into_iter()
-        .filter(|group| group.len() > 1)
-        .map(|group| {
-            group[2..].into_iter().flat_map(|row| {
-                let entry = Entry {
-                    fajr: row.2.parse().ok()?,
-                    shurooq: row.3.parse().ok()?,
-                    dhuhr: row.4.parse().ok()?,
-                    asr_shafi: row.5.parse().ok()?,
-                    asr_hanafi: row.6.parse().ok()?,
-                    maghrib: row.7.parse().ok()?,
-                    isha: row.8.parse().ok()?,
-                };
-                Some(entry)
-            }).collect::<Vec<_>>()
-        })
-        .flatten()
-        .collect::<Vec<_>>();
+        .filter(|group| group.len() > 1);
 
-    // Return months
-    Ok(months)
+    // Map each row into entry and add to entries
+    let mut entries = Vec::with_capacity(365);
+    for (i, group) in groups.into_iter().enumerate() {
+        let slice = if i == 0 {
+            &group[1..]
+        } else {
+            &group[2..]
+        };
+        
+        for row in slice {
+            let entry = Entry {
+                day: row.0.parse()?,
+                fajr: row.2.parse()?,
+                shurooq: row.3.parse()?,
+                dhuhr: row.4.parse()?,
+                asr_shafi: row.5.parse()?,
+                asr_hanafi: row.6.parse()?,
+                maghrib: row.7.parse()?,
+                isha: row.8.parse().ok(),
+            };
+            entries.push(entry);
+        }
+    }
+    
+    // Return entries
+    Ok(entries)
 }
